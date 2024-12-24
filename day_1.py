6fd33a73e2d726de0618a9ef19d88f880f1b4457
Jack <jemeslow@gmail.com>
2024-12-23 19:50:11 -0500

    Update day 1 to complete part 2

--- day_1.py
+++ day_1.py
@@ -7,6 +7,20 @@ data[0] = np.sort(data[0])
 # Sort 'right side
 data[1] = np.sort(data[1])
 # Sum and return differences
-print(np.sum(np.abs(data[1] - data[0])))
+print("Sum of differences: " + str(np.sum(np.abs(data[1] - data[0]))))
 
 # Start part 2
+
+occurences = {}
+
+for i in data[1]:
+    if i in occurences:
+        occurences[i] +=1
+    else:
+        occurences[i] = 1
+
+similarity_score = 0
+for i in data[0]:
+    if i in occurences:
+        similarity_score += i * occurences[i]
+print(f'The similarity score for this input is: {similarity_score}')
